b69d60aa64e095b9c119e521c992ded6c598f422
Jyotpreet Kaur <justechere@gmail.com>
2024-05-18 18:02:11 +0530

    added

---
+++ videoconferencingapp/agora-rn-uikit/src/Reducer/UserOffline.ts
@@ -0,0 +1,24 @@
+import {ActionType, ContentStateInterface} from '../Contexts/RtcContext';
+
+export default function UserOffline(
+  state: ContentStateInterface,
+  action: ActionType<'UserOffline'>,
+) {
+  // let updatedRenderList = {
+  //   ...state.defaultContent,
+  // };
+  // //don't delete user data from renderlist
+  // //we will update user data with {offline:true} from RTM user left event
+  // if (updatedRenderList[action.value[0]]) {
+  //   delete updatedRenderList[action.value[0]];
+  // }
+  const updatedActiveUids = [...state.activeUids].filter(
+    (uid) => uid !== action.value[0],
+  );
+  const stateUpdate: Partial<ContentStateInterface> = {
+    defaultContent: state.defaultContent,
+    activeUids: updatedActiveUids,
+  };
+
+  return stateUpdate;
+}
